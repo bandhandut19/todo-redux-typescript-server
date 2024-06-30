faf22e81f0852b9641022287c5ba12e39af47a2c
bandhandut19 <bondon21081@gmail.com>
2024-06-30 21:31:45 +0600

    successfully connected with mongodb

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
-
 const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb://localhost:27017`;
+const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -18,16 +17,11 @@ const client = new MongoClient(uri, {
 });
 
 const run = async () => {
-  // await client.connect();
   try {
+    await client.connect();
+    console.log('Connected to MongoDB');
     const db = client.db('todo');
     const taskCollection = db.collection('tasks');
-
-    // app.get('/tasks', async (req, res) => {
-    //   const cursor = taskCollection.find({});
-    //   const tasks = await cursor.toArray();
-    //   res.send({ status: true, data: tasks });
-    // });
 
     app.get('/tasks', async (req, res) => {
       let query = {};
@@ -48,21 +42,17 @@ const run = async () => {
     app.get('/task/:id', async (req, res) => {
       const id = req.params.id;
       const result = await taskCollection.findOne({ _id: ObjectId(id) });
-      // console.log(result);
       res.send(result);
     });
 
     app.delete('/task/:id', async (req, res) => {
       const id = req.params.id;
       const result = await taskCollection.deleteOne({ _id: ObjectId(id) });
-      // console.log(result);
       res.send(result);
     });
 
-    // status update
     app.put('/task/:id', async (req, res) => {
       const id = req.params.id;
-      console.log(id);
       const task = req.body;
       const filter = { _id: ObjectId(id) };
       const updateDoc = {
@@ -77,7 +67,9 @@ const run = async () => {
       const result = await taskCollection.updateOne(filter, updateDoc, options);
       res.json(result);
     });
-  } finally {
+
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
   }
 };
 
